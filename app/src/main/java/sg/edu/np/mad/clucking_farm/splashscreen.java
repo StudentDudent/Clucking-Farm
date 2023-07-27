4aad4f836695468554bb62574c527b7a99540f95
StudentDudent <116237984+StudentDudent@users.noreply.github.com>
2023-07-27 17:35:13 +0800

    Updated version but dont pull or clone yet

---
+++ app/src/main/java/sg/edu/np/mad/clucking_farm/splashscreen.java
@@ -0,0 +1,47 @@
+package sg.edu.np.mad.clucking_farm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splashscreen extends AppCompatActivity {
+
+    ProgressBar pb;
+    int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent secondActivityIntent = new Intent(
+                this, MainActivity.class
+        );
+        startActivity(secondActivityIntent);
+
+        setContentView(R.layout.activity_splashscreen);
+        progressbar();
+    }
+
+    public void progressbar()
+    {
+        pb = findViewById(R.id.progressBar);
+
+        final Timer t = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run()
+            {
+                counter++;
+                pb.setProgress(counter);
+
+                if(counter == 100)
+                    t.cancel();
+
+            }
+        };
+        t.schedule(tt,0,100);
+    }
+}
